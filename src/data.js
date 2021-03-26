c6a90fe043ac564deffd715886d8fdc457fec4be
noeliarios <noelia.rivers.1887@gmail.com>
2021-03-25 21:19:06 -0300

    start

---
+++ src/data.js
@@ -0,0 +1,65 @@
+const data = [
+  {
+    text: "Wrote my first line of code",
+    date: "November 06 2018",
+    category: {
+      tag: "code",
+      color: "blue",
+    },
+    link: {
+      url: "https://github.com/NoeliaRios/Honey",
+      text: "Read more",
+    },
+  },
+  {
+    text: "First personal coding project using MovieDB API",
+    date: "June 01 2019",
+    category: {
+      tag: "code",
+      color: "blue",
+    },
+    link: {
+      url: "https://github.com/NoeliaRios/TP3-Movie-Library",
+      text: "Read more",
+    },
+  },
+  {
+    text: "Got my first IT job as a Frontend Developer",
+    date: "October 01 2019",
+    category: {
+      tag: "jobs",
+      color: "orange",
+    },
+    link: {
+      url: "http://www.dosmonos.com/",
+      text: "Read more",
+    },
+  },
+  {
+    text: "Completed a Backend course at CourseIT",
+    date: "February 16 2021",
+    category: {
+      tag: "Education",
+      color: "green",
+    },
+    link: {
+      url: "https://www.courseit.io/",
+      text: "Read more",
+    },
+  },
+  {
+    text: "Computer Science at Buenos Aires University",
+    date: "April 01 2021",
+    category: {
+      tag: "Education",
+      color: "green",
+    },
+    link: {
+      url:
+        "https://exactas.uba.ar/ensenanza/carreras-de-grado/ciencias-de-la-computacion/",
+      text: "Read more",
+    },
+  },
+];
+
+export default data;
